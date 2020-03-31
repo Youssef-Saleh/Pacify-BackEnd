cb0b9b5e377fda88b877e5e5a2db01a561b6be0c
AhmedEldib <masterahmed25@hotmial.com>
2020-04-01 00:18:45 +0200

    Updated the Database with the genres

--- Database Seeds/seeding/insertproperty.js
+++ Database Seeds/seeding/insertproperty.js
@@ -5,14 +5,77 @@ mongoose.connect('mongodb://localhost:27017/testpacify');
 
 var properties = [
     new Property({
-        Name: "Egypt",
-        type: "Country"
+        Name: "Arabic",
+        type: "Genre"
 
+    }),
+    new Property({
+        Name: "Blues",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "Chill",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "Classical",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "Country",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "Electronic",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "Gaming",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "HipHop",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "Jazz",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "Kids",
+        type: "Genre"
     }),
     new Property({
         Name: "Pop",
         type: "Genre"
-    })
+
+    }),
+    new Property({
+        Name: "Punk",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "R&B",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "Rock",
+        type: "Genre"
+    }),
+    new Property({
+        Name: "Travel",
+        type: "Genre"
+
+    }),
+    new Property({
+        Name: "TV&Movies",
+        type: "Genre"
+    }),
 ];
 
 var counter = 0;
