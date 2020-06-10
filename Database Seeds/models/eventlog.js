52b95d80836f1cc67e0e89277ae6ebb276a07eea
AhmedEldib <masterahmed25@hotmial.com>
2020-06-10 21:11:10 +0200

    remodeled the eventlog model to have day month year seperate instead of just 1 object with d/m/y

--- Database Seeds/models/eventlog.js
+++ Database Seeds/models/eventlog.js
@@ -5,7 +5,9 @@ var schema = new Schema({
     //_id: mongoose.Schema.Types.ObjectId,
     Performer:{ type: String, required: true},
     Affected: { type: String, required: true}, 
-    Time: {type: Date, default: Date.now()},
+    Year: {type: Number, default: (new Date).getFullYear()},
+    Month: {type: Number, default: (new Date).getMonth()},
+    Day: {type: Number, default: (new Date).getDay()},
     Type: {type: String, required: true},
     Object: {type: String, default: null}
 
