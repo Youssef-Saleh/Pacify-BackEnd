4976a0d0e228fd83c7137117b7788be19c9bb5c9
AhmedEldib <masterahmed25@hotmial.com>
2020-06-10 20:00:21 +0200

    fixed an error in exception handling

--- middlewares/token_auth.js
+++ middlewares/token_auth.js
@@ -2,21 +2,23 @@ const jwt = require('jsonwebtoken');
 const authVar = require('../env_variables/env_vars.json').KEY
 
 module.exports = (req, res, next) => {
-    const token_header = req.headers.authorization.split(" ")[1];
-    //console.log(token_header)
+    try{
+        const token_header = req.headers.authorization.split(" ")[1];
+        //console.log(token_header)
 
-    if(typeof token_header !== 'undefined') {
-        try{
-        token = jwt.verify(token_header, authVar);
-        req.userId = jwt.decode(token_header)._id; 
-        next();
-        }catch(error){
-            return res.sendStatus(403).json({
+        if(typeof token_header !== 'undefined') {
+            
+            token = jwt.verify(token_header, authVar);
+            req.userId = jwt.decode(token_header)._id; 
+            next();
+            
+        } else {
+            res.sendStatus(403).json({
                 msg: 'Unauthorized access'
             })
         }
-    } else {
-        res.sendStatus(403).json({
+    } catch(error){
+        return res.sendStatus(403).json({
             msg: 'Unauthorized access'
         })
     }
